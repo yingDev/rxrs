b780b3af6ddfc0309d667f4c81f5cf69ee405570
yingDev <ying@yingDev.com>
2018-03-21 08:55:28 +0800

    use Mss

--- src/fac/timer.rs
+++ src/fac/timer.rs
@@ -8,46 +8,56 @@ use subref::*;
 use fac::*;
 use scheduler::Scheduler;
 use util::mss::*;
+use std::marker::PhantomData;
 
-pub fn timer(delay: u64, period: impl Into<Option<u64>>, scheduler: Arc<impl Scheduler+Send+Sync+'static>) -> impl Observable<'static, usize, Yes>
+pub fn timer(delay: u64, period: impl Into<Option<u64>>, scheduler: Arc<impl Scheduler<SSA=No>+Send+Sync+'static>) -> impl Observable<'static, usize, No+'static>
 {
     timer_dur(Duration::from_millis(delay), period.into().map(|v| Duration::from_millis(v)), scheduler)
 }
 
-pub fn timer_dur(delay: Duration, period: impl Into<Option<Duration>>, scheduler: Arc<impl Scheduler+Send+Sync+'static>) -> impl Observable<'static, usize, Yes>
+pub fn timer_ss(delay: u64, period: impl Into<Option<u64>>, scheduler: Arc<impl Scheduler<SSA=Yes>+Send+Sync+'static>) -> impl Observable<'static, usize, Yes>
+{
+    timer_dur_ss(Duration::from_millis(delay), period.into().map(|v| Duration::from_millis(v)), scheduler)
+}
+
+macro_rules! fnbody(($s:ty, $delay:ident, $sch:ident, $period:ident, $o:ident) => {{
+    let scheduler2 = $sch.clone();
+    let sig = SubRef::signal();
+
+    $sch.schedule_after($delay, Mss::<$s,_>::new(move ||
+    {
+        if $o._is_closed() || sig.disposed() { return sig.clone(); }
+
+        let count = AtomicUsize::new(0);
+        $o.next(count.fetch_add(1, Ordering::SeqCst));
+        if $period.is_none() { return sig.clone(); }
+
+        scheduler2.schedule_periodic($period.unwrap(), sig.clone(), Mss::<$s,_>::new(move ||
+        {
+            if $o._is_closed() || sig.disposed()
+            {
+                sig.unsub();
+                return;
+            }
+            $o.next(count.fetch_add(1, Ordering::SeqCst));
+        }))
+    }))
+}});
+
+pub fn timer_dur(delay: Duration, period: impl Into<Option<Duration>>, scheduler: Arc<impl Scheduler<SSA=No>+Send+Sync+'static>) -> impl Observable<'static, usize, No+'static>
 {
     let period = period.into();
-
-    create_sso(move |o: Mss<Yes, Box<Observer<usize>+'static>>|
-    {
-        let scheduler2 = scheduler.clone();
-        let sig = SubRef::signal();
-
-        scheduler.schedule_after(delay, move ||
-        {
-            if o._is_closed() || sig.disposed() { return sig.clone(); }
-
-            let count = AtomicUsize::new(0);
-            o.next(count.fetch_add(1, Ordering::SeqCst));
-            if period.is_none() { return sig.clone(); }
-
-            scheduler2.schedule_periodic(period.unwrap(), sig.clone(), move ||
-            {
-                if o._is_closed() || sig.disposed()
-                {
-                    sig.unsub();
-                    return;
-                }
-                o.next(count.fetch_add(1, Ordering::SeqCst));
-            })
-        })
-
+    create_boxed(move |o: Mss<No, Box<Observer<usize>+'static>>| {
+        fnbody!(No, delay, scheduler, period, o)
     })
 }
 
-pub fn timer_once(delay: u64, scheduler: Arc<impl Scheduler+Send+Sync+'static>) -> impl Observable<'static, usize, Yes>
+pub fn timer_dur_ss(delay: Duration, period: impl Into<Option<Duration>>, scheduler: Arc<impl Scheduler<SSA=Yes>+Send+Sync+'static>) -> impl Observable<'static, usize, Yes>
 {
-    timer(delay, None, scheduler)
+    let period = period.into();
+    create_sso(move |o: Mss<Yes, Box<Observer<usize>+'static>>| {
+        fnbody!(Yes, delay, scheduler, period, o)
+    })
 }
 
 #[cfg(test)]
@@ -62,27 +72,28 @@ mod test
     fn timer_basic()
     {
         timer(100, 100, Arc::new(ImmediateScheduler::new())).take(10).subf(|v|  println!("{}", v));
+
         timer(100, None, Arc::new(ImmediateScheduler::new()));
 
-        timer_once(100, Arc::new(ImmediateScheduler::new())).subf(|v| println!("once..."));
+
     }
 
     #[test]
     fn new_thread()
     {
-        use ::std::sync::Condvar;
-        use ::std::sync::Mutex;
-
-        let pair = Arc::new((Mutex::new(false), Condvar::new()));
-        let pair2 = pair.clone();
-
-        timer(100, 100, NewThreadScheduler::get()).take(10).subf((|v|  println!("{}", v), (), move || {
-            let &(ref lock, ref cvar) = &*pair2;
-            *lock.lock().unwrap() = true;
-                cvar.notify_one();
-        }));
-
-        let &(ref lock, ref cvar) = &*pair;
-        cvar.wait(lock.lock().unwrap());
+//        use ::std::sync::Condvar;
+//        use ::std::sync::Mutex;
+//
+//        let pair = Arc::new((Mutex::new(false), Condvar::new()));
+//        let pair2 = pair.clone();
+//
+//        timer(100, 100, NewThreadScheduler::get()).take(10).subf((|v|  println!("{}", v), (), move || {
+//            let &(ref lock, ref cvar) = &*pair2;
+//            *lock.lock().unwrap() = true;
+//                cvar.notify_one();
+//        }));
+//
+//        let &(ref lock, ref cvar) = &*pair;
+//        cvar.wait(lock.lock().unwrap());
     }
 }
--- src/scheduler.rs
+++ src/scheduler.rs
@@ -3,27 +3,22 @@ use subref::SubRef;
 use std::sync::Arc;
 use std::sync::Once;
 use std::sync::ONCE_INIT;
+use util::mss::*;
 
 //todo: facade
 
 pub trait Scheduler
 {
-    fn schedule(&self, act: impl Send+'static+FnOnce()->SubRef) -> SubRef;
-    fn schedule_after(&self, due: Duration, act: impl Send+'static+FnOnce()->SubRef) -> SubRef;
+    type SSA:?Sized;
 
-    fn schedule_periodic(&self, period: Duration,sigStop: SubRef, act: impl Send+'static+Fn()) -> SubRef
+    fn schedule(&self, act: Mss<Self::SSA,impl 'static+FnOnce()->SubRef>) -> SubRef;
+    fn schedule_after(&self, due: Duration, act: Mss<Self::SSA, impl 'static+FnOnce()->SubRef>) -> SubRef;
+
+    fn schedule_periodic(&self, period: Duration,sigStop: SubRef, act: Mss<Self::SSA, impl 'static+Fn()>) -> SubRef
     {
         unimplemented!()
     }
-    fn schedule_long_running(&self, sigStop: SubRef, act: impl Send+'static+FnOnce()) -> SubRef
-    {
-        if sigStop.disposed() { return sigStop; }
-        self.schedule(||{
-            if sigStop.disposed() { return sigStop; }
-            act();
-            sigStop
-        })
-    }
+    fn schedule_long_running(&self, sigStop: SubRef, act: Mss<Self::SSA, impl 'static+FnOnce()>) -> SubRef;
 }
 
 pub struct ImmediateScheduler;
@@ -35,18 +30,31 @@ impl ImmediateScheduler
 
 impl Scheduler for ImmediateScheduler
 {
-    fn schedule(&self, act: impl Send+'static+FnOnce()->SubRef) -> SubRef
+    type SSA = No;
+
+    fn schedule(&self, act: Mss<Self::SSA,impl 'static+FnOnce()->SubRef>) -> SubRef
     {
-        act()
+        (act.into_inner())()
     }
 
-    fn schedule_after(&self, due: Duration, act: impl Send+'static+FnOnce()->SubRef) -> SubRef
+    fn schedule_after(&self, due: Duration, act: Mss<Self::SSA,impl 'static+FnOnce()->SubRef>) -> SubRef
     {
         ::std::thread::sleep(due);
-        act()
+        (act.into_inner())()
     }
 
-    fn schedule_periodic(&self, period: Duration, sigStop: SubRef, act: impl Send+'static+Fn()) -> SubRef
+    fn schedule_long_running(&self, sigStop: SubRef, act: Mss<Self::SSA, impl 'static+FnOnce()>) -> SubRef
+    {
+        if sigStop.disposed() { return sigStop; }
+        let act = act.into_inner();
+        self.schedule(Mss::no(||{
+            if sigStop.disposed() { return sigStop; }
+            act();
+            sigStop
+        }))
+    }
+
+    fn schedule_periodic(&self, period: Duration, sigStop: SubRef, act: Mss<Self::SSA,impl 'static+Fn()>) -> SubRef
     {
         while ! sigStop.disposed()
         {
@@ -60,62 +68,62 @@ impl Scheduler for ImmediateScheduler
         sigStop
     }
 }
-
-static mut _NewThreadScheduler: Option<Arc<NewThreadScheduler>> = None;
-static _NewThreadScheduler_INIT: Once = ONCE_INIT;
-
-pub struct NewThreadScheduler
-{
-
-}
-impl NewThreadScheduler
-{
-    pub fn get() -> Arc<NewThreadScheduler> {
-        _NewThreadScheduler_INIT.call_once(|| {
-            unsafe { _NewThreadScheduler = Some(Arc::new(NewThreadScheduler{})); }
-        });
-        unsafe { _NewThreadScheduler.as_ref().unwrap().clone() }
-    }
-}
-
-impl Scheduler for NewThreadScheduler
-{
-    fn schedule(&self, act: impl Send+'static+FnOnce()->SubRef) -> SubRef
-    {
-        let unsub = SubRef::signal();
-        let unsub2 = unsub.clone();
-
-       ::std::thread::spawn(move ||{
-           unsub2.add(act());
-       });
-
-       unsub
-    }
-
-    fn schedule_after(&self, due: Duration, act: impl Send+'static+FnOnce()->SubRef) -> SubRef
-    {
-        let unsub = SubRef::signal();
-        let unsub2 = unsub.clone();
-
-        ::std::thread::spawn(move ||{
-            ::std::thread::sleep(due);
-            if ! unsub2.disposed() { unsub2.add(act()); }
-        });
-
-        unsub
-    }
-
-    fn schedule_periodic(&self, period: Duration, sigStop: SubRef, act: impl Send+'static+Fn()) -> SubRef
-    {
-        let stop = sigStop.clone();
-        ::std::thread::spawn(move ||
-        {
-            while ! stop.disposed(){
-                ::std::thread::sleep(period);
-                if stop.disposed() { break; }
-                act();
-            }
-        });
-        sigStop
-    }
-}
+//
+//static mut _NewThreadScheduler: Option<Arc<NewThreadScheduler>> = None;
+//static _NewThreadScheduler_INIT: Once = ONCE_INIT;
+//
+//pub struct NewThreadScheduler
+//{
+//
+//}
+//impl NewThreadScheduler
+//{
+//    pub fn get() -> Arc<NewThreadScheduler> {
+//        _NewThreadScheduler_INIT.call_once(|| {
+//            unsafe { _NewThreadScheduler = Some(Arc::new(NewThreadScheduler{})); }
+//        });
+//        unsafe { _NewThreadScheduler.as_ref().unwrap().clone() }
+//    }
+//}
+//
+//impl Scheduler for NewThreadScheduler
+//{
+//    fn schedule(&self, act: impl Send+'static+FnOnce()->SubRef) -> SubRef
+//    {
+//        let unsub = SubRef::signal();
+//        let unsub2 = unsub.clone();
+//
+//       ::std::thread::spawn(move ||{
+//           unsub2.add(act());
+//       });
+//
+//       unsub
+//    }
+//
+//    fn schedule_after(&self, due: Duration, act: impl Send+'static+FnOnce()->SubRef) -> SubRef
+//    {
+//        let unsub = SubRef::signal();
+//        let unsub2 = unsub.clone();
+//
+//        ::std::thread::spawn(move ||{
+//            ::std::thread::sleep(due);
+//            if ! unsub2.disposed() { unsub2.add(act()); }
+//        });
+//
+//        unsub
+//    }
+//
+//    fn schedule_periodic(&self, period: Duration, sigStop: SubRef, act: impl Send+'static+Fn()) -> SubRef
+//    {
+//        let stop = sigStop.clone();
+//        ::std::thread::spawn(move ||
+//        {
+//            while ! stop.disposed(){
+//                ::std::thread::sleep(period);
+//                if stop.disposed() { break; }
+//                act();
+//            }
+//        });
+//        sigStop
+//    }
+//}

c15165ac31ce1d4ba8107e818198c907727f0557
yingDev <ying@yingDev.com>
2018-03-30 07:49:17 +0800

    refactor

--- src/fac/create.rs
+++ src/fac/create.rs
@@ -5,55 +5,80 @@ use observable::Observer;
 use subref::IntoSubRef;
 use util::mss::*;
 use std::cell::UnsafeCell;
+use std::cell::RefCell;
+use std::rc::Rc;
 
-pub fn empty<'a>() -> impl Observable<'a, (), No+'static>
+
+
+pub struct EmptyObservable<'a,V>(PhantomData<(&'a(), *const V)>);
+impl<'a,V> Clone for EmptyObservable<'a,V>
 {
-    create(|o|{
+    fn clone(&self) -> EmptyObservable<'a,V>
+    {
+        EmptyObservable(PhantomData)
+    }
+}
+impl<'a, V> Observable<'a, V, No> for EmptyObservable<'a,V>
+{
+    fn sub(&self, o: Mss<No, impl Observer<V>+'a>) -> SubRef
+    {
+        o.complete();
+        SubRef::empty()
+    }
+}
+
+pub fn empty<'a, V>() -> EmptyObservable<'a,V>
+{
+    EmptyObservable(PhantomData)
+}
+
+
+pub fn just<'a, V:'a+Clone>(v:V) -> impl Observable<'a, V, No+'static>+Clone
+{
+    create(move |o|{
+        o.next(v.clone());
         o.complete();
     })
 }
-pub fn empty_of<'a,V:'a>() -> impl Observable<'a, V, No+'static>
-{
-    create(|o|{
-        o.complete();
-    })
-}
 
-pub fn create<'a:'b, 'b, V:'a, F, R>(sub: F) -> impl Observable<'a, V, No+'static>+'b where F: FnMut(Mss<No, &(Observer<V>+'a)>) -> R, R: IntoSubRef+'static
+#[derive(Clone)]
+pub struct LocalObservable<'a, V, F, R>(RefCell<F>, PhantomData<(&'a(), *const R, *const V)>) where F: 'a+FnMut(Mss<No, &(Observer<V>+'a)>) -> R, R: IntoSubRef+'static;
+impl<'a:'b,'b,V, F, R> Observable<'a, V, No> for LocalObservable<'a, V, F, R> where F: 'a+FnMut(Mss<No, &(Observer<V>+'a)>) -> R, R: IntoSubRef+'static
 {
-    struct LocalObservable<'a, V, F, R>(F, PhantomData<(&'a(), *const R, *const V)>) where F: 'a+Fn(Mss<No, &(Observer<V>+'a)>) -> R, R: IntoSubRef+'static;
-    impl<'a:'b,'b,V, F, R> Observable<'a, V, No> for LocalObservable<'a, V, F, R> where F: 'a+Fn(Mss<No, &(Observer<V>+'a)>) -> R, R: IntoSubRef+'static
+    fn sub(&self, o: Mss<No, impl Observer<V>+'a>) -> SubRef
     {
-        fn sub(&self, o: Mss<No, impl Observer<V>+'a>) -> SubRef
-        {
-            let sub = (self.0)(Mss::no(&o.into_inner()));
-            IntoSubRef::into(sub)
-        }
-    }
-    let cell = UnsafeCell::new(sub);
-    unsafe {
-        LocalObservable(move |o| (*cell.get())(o), PhantomData)
+        let mut f = self.0.borrow_mut();
+        let sub = f.call_mut((Mss::no(&o.into_inner()),));
+
+        IntoSubRef::into(sub)
     }
 }
 
-pub fn create_boxed<'a:'b, 'b, V:'a, F, R>(sub: F) -> impl Observable<'a, V, No+'static>+'b where F: FnMut(Mss<No, Box<Observer<V>+'a>>) -> R, R: IntoSubRef+'static
+pub fn create<'a:'b, 'b, V:'a, F, R>(sub: F) -> LocalObservable<'a,V,F,R> where F: FnMut(Mss<No, &(Observer<V>+'a)>) -> R, R: IntoSubRef+'static
 {
-    struct LocalObservable<'a, V, F, R>(F, PhantomData<(&'a(), R, V)>) where F: 'a+Fn(Mss<No, Box<Observer<V>+'a>>) -> R, R: IntoSubRef+'static;
-    impl<'a:'b, 'b, V:'a, F, R> Observable<'a, V, No> for LocalObservable<'a, V, F, R> where F: 'a+Fn(Mss<No, Box<Observer<V>+'a>>) -> R, R: IntoSubRef+'static
-    {
-        fn sub(&self, o: Mss<No, impl Observer<V>+'a>) -> SubRef
-        {
-            let sub = (self.0)(o.into_boxed());
-            IntoSubRef::into(sub)
-        }
-    }
+    LocalObservable(RefCell::new(sub), PhantomData)
+}
 
-    let cell = UnsafeCell::new(sub);
-    unsafe { LocalObservable(move |o| (*cell.get())(o),PhantomData) }
+#[derive(Clone)]
+pub struct LocalBoxedObservable<'a, V, F, R>(RefCell<F>, PhantomData<(&'a(), R, V)>) where F: 'a+FnMut(Mss<No, Box<Observer<V>+'a>>) -> R, R: IntoSubRef+'static;
+impl<'a:'b, 'b, V:'a, F, R> Observable<'a, V, No> for LocalBoxedObservable<'a, V, F, R> where F: 'a+FnMut(Mss<No, Box<Observer<V>+'a>>) -> R, R: IntoSubRef+'static
+{
+    fn sub(&self, o: Mss<No, impl Observer<V>+'a>) -> SubRef
+    {
+        let mut f = self.0.borrow_mut();
+        let sub = f.call_mut((o.into_boxed(),));
+        IntoSubRef::into(sub)
+    }
+}
+
+pub fn create_boxed<'a:'b, 'b, V:'a, F, R>(sub: F) -> LocalBoxedObservable<'a,V,F,R> where F: FnMut(Mss<No, Box<Observer<V>+'a>>) -> R, R: IntoSubRef+'static
+{
+    LocalBoxedObservable(RefCell::new(sub), PhantomData)
 }
 
 pub fn create_sso<'a, V, F, R>(sub: F) -> impl Observable<'static, V, Yes> where F: Send+Sync+Fn(Mss<Yes, Box<Observer<V>+'static>>) -> R, R: IntoSubRef
 {
+    #[derive(Copy,Clone)]
     struct SendSyncObserverObservable<V, F, R>(F, PhantomData<(R, V)>) where F: Send+Sync+Fn(Mss<Yes, Box<Observer<V>+'static>>) -> R, R: IntoSubRef;
     impl<V, F, R> Observable<'static, V, Yes> for SendSyncObserverObservable<V, F, R> where F: Send+Sync+Fn(Mss<Yes, Box<Observer<V>+'static>>) -> R, R: IntoSubRef
     {
@@ -66,6 +91,35 @@ pub fn create_sso<'a, V, F, R>(sub: F) -> impl Observable<'static, V, Yes> where
     SendSyncObserverObservable(sub, PhantomData)
 }
 
+pub fn range<'a>(start:i32, len: usize) -> RangeObservable
+{
+    RangeObservable{ start, end: start + (len as i32) }
+}
+
+#[derive(Copy,Clone)]
+pub struct RangeObservable
+{
+    start: i32,
+    end: i32,
+}
+impl<'a> Observable<'a, i32, No> for RangeObservable
+{
+    fn sub(&self, o: Mss<No, impl Observer<i32>+'a>) -> SubRef
+    {
+        let mut i = self.start;
+        let end = self.end;
+
+        while i != end {
+            if o._is_closed() { return SubRef::empty(); }
+            o.next(i);
+            i += 1;
+        }
+        if o._is_closed() { return SubRef::empty(); }
+        o.complete();
+
+        SubRef::empty()
+    }
+}
 
 #[cfg(test)]
 mod test
@@ -76,6 +130,7 @@ mod test
     use std::sync::atomic::Ordering;
     use std::time::Duration;
     use std::sync::Arc;
+    use test_fixture::SimpleObservable;
 
     #[test]
     fn basic()
@@ -113,5 +168,17 @@ mod test
 
         ::std::thread::sleep(Duration::from_millis(100));
         assert_eq!(i.load(Ordering::SeqCst), 3);
+    }
+
+    #[test]
+    fn range_test()
+    {
+
+        let i = range(0, 3);
+        let mut count = 0;
+        i.subf(|v| count += 1);
+
+
+        assert_eq!(count, 3);
     }
 }

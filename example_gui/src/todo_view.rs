81e0700af7ff7ac41d05d2c4de6865f80ddd21af
yingDev <ying@yingDev.com>
2018-04-16 10:33:10 +0800

    unbind

--- example_gui/src/todo_view.rs
+++ example_gui/src/todo_view.rs
@@ -8,6 +8,8 @@ use rx::observable::*;
 use rx::op::*;
 use todo_models::*;
 use rx::behaviour_subject_nss::*;
+use rx::subject_nss::Subject;
+
 
 const FILTER_BTNS: &'static [(Filter, &'static str)] = {
     use ::todo_models::Filter::*;
@@ -28,7 +30,7 @@ struct State
     lb_empty: gtk::Label,
 
     filter_btns: Rc<HashMap<Filter, gtk::RadioButton>>,
-    sig_reset: Rc<Subject<()>>
+    sig_reset: Rc<Subject<'static,()>>
 }
 
 
@@ -49,62 +51,66 @@ impl TodoView
         TodoView{ state: Rc::new(State{ input, list, filter_btns, lb_items_left, btn_clear_completed, lb_empty, sig_reset: Rc::new(Subject::new()) }) }
     }
 
-    pub fn bind(&self, vm: Rc<TodoViewModel>)
+    pub fn bind(&self, vm: impl Into<Option<Rc<TodoViewModel>>>)
     {
         let state = self.state.clone();
-        let sig_reset = state.sig_reset();
+        state.sig_reset.next(());
 
-        sig_reset.next(());
+        if let Some(vm) = vm.into() {
+            let sig_reset = Rc::new(state.sig_reset.clone().first());
 
-        state.filter_btns.iter().for_each(|(f, btn)| {
-            signal!(btn,connect_toggled).subf(byclone!(vm, f, btn => move |_| {
-                if btn.get_active() {
-                    vm.set_filter(f);
-                }
-            }));
-        });
-
-        signal!(state.btn_clear_completed,connect_clicked).subf(byclone!(vm => move |_| {
-            vm.clear_completed();
-        }));
-
-        vm.filter().subf(byclone!(state=> move |f| {
-            state.filter_btns[&f].set_active(true);
-        }));
-
-        vm.items_left().subf(byclone!(state => move |i| {
-            state.lb_items_left.set_markup(&format!("{}", i));
-        }));
-
-        vm.changes().subf(byclone!(vm, state => move |_|
-        {
-            state.list.get_children().iter().for_each(|w| state.list.remove(w));
-            let mut n=0;
-            vm.items().subf(|it: ItemRef|
-            {
-                let row = gtk::ListBoxRow::new();
-                let item = gtk::CheckButton::new_with_label(&it.title);
-                item.set_active(it.completed);
-                row.add(&item);
-                if let Some(ctx) = row.get_style_context() {
-                    ctx.add_class("todo_item");
-                }
-                state.list.prepend(&row);
-                n += 1;
-                signal!(item, connect_toggled).subf(byclone!(vm,it => move |_| vm.toggle_item(it.id)));
+            state.filter_btns.iter().for_each(|(f, btn)| {
+                signal!(btn,connect_toggled).take_until(sig_reset.clone()).subf(byclone!(vm, f, btn => move |_| {
+                    if btn.get_active() {
+                        vm.set_filter(f);
+                    }
+                }));
             });
 
-            state.lb_empty.set_visible(n == 0);
-            state.list.show_all();
-        }));
+            signal!(state.btn_clear_completed,connect_clicked).take_until(sig_reset.clone()).subf(byclone!(vm => move |_| {
+                vm.clear_completed();
+            }));
 
-        signal!(state.input, connect_activate, it => it.get_text())
-            .filter(|v:&Option<String>| v.as_ref().filter(|s| s.len() > 0).is_some())
-            .map(|v| v.unwrap())
-            .subf(byclone!(state, vm => move |v: String|
-        {
-            state.input.set_text("");
-            vm.add_item(v);
-        }));
+            vm.filter().take_until(sig_reset.clone()).subf(byclone!(state=> move |f| {
+                state.filter_btns[&f].set_active(true);
+            }));
+
+            vm.items_left().take_until(sig_reset.clone()).subf(byclone!(state => move |i| {
+                state.lb_items_left.set_markup(&format!("{}", i));
+            }));
+
+            vm.changes().take_until(sig_reset.clone()).subf(byclone!(vm, state, sig_reset => move |_|
+            {
+                state.list.get_children().iter().for_each(|w| state.list.remove(w));
+                let mut n=0;
+                vm.items().subf(|it: ItemRef|
+                {
+                    let row = gtk::ListBoxRow::new();
+                    let item = gtk::CheckButton::new_with_label(&it.title);
+                    item.set_active(it.completed);
+                    row.add(&item);
+                    if let Some(ctx) = row.get_style_context() {
+                        ctx.add_class("todo_item");
+                    }
+                    state.list.prepend(&row);
+                    n += 1;
+                    signal!(item, connect_toggled).take_until(sig_reset.clone()).subf(byclone!(vm,it => move |_| vm.toggle_item(it.id)));
+                });
+
+                state.lb_empty.set_visible(n == 0);
+                state.list.show_all();
+            }));
+
+            signal!(state.input, connect_activate, it => it.get_text())
+                .take_until(sig_reset.clone())
+                .filter(|v:&Option<String>| v.as_ref().filter(|s| s.len() > 0).is_some())
+                .map(|v| v.unwrap())
+                .subf(byclone!(state, vm => move |v: String|
+            {
+                state.input.set_text("");
+                vm.add_item(v);
+            }));
+        }
+
     }
 }

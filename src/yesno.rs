3dfc28954b220e03bcd4862990390c4e7e1a5969
ying <ying@yingDev.com>
2018-10-08 05:49:13 +0800

    derive

--- src/yesno.rs
+++ src/yesno.rs
@@ -1,5 +1,9 @@
-pub trait YesNo : 'static { const VALUE: bool; }
+pub trait YesNo : Send+Sync+Clone+ 'static { const VALUE: bool; }
+
+#[derive(Copy, Clone)]
 pub struct YES;
+#[derive(Copy, Clone)]
 pub struct NO;
+
 impl YesNo for YES { const VALUE: bool = true; }
 impl YesNo for NO { const VALUE: bool = false; }

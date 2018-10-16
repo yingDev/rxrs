3ad956cb0d75d4905ee0120c96088303a064e198
ying <ying@yingDev.com>
2018-10-17 01:51:56 +0800

    ...

--- src/fac/of.rs
+++ src/fac/of.rs
@@ -5,13 +5,12 @@ pub struct Of<V> { v: V }
 impl<V> Of<V>
 {
     pub fn value(v: V) -> Of<V> { Of{ v } }
+    pub fn value_dyn<'o,'a>(v: V)  -> Box<Observable<'o, NO, Ref<V>>+'a> where V:'o+'a { Self::value(v).into_dyn() }
 }
 
 impl<'o, V: 'o> Observable<'o, NO, Ref<V>, Ref<()>> for Of<V>
 {
-    fn sub(&self,
-           next: impl FnNext<NO, Ref<V>> + 'o,
-           ec: impl FnErrComp<NO, Ref<()>> + 'o) -> Unsub<'o, NO> where Self: Sized
+    fn sub(&self, next: impl FnNext<NO, Ref<V>> + 'o, ec: impl FnErrComp<NO, Ref<()>> + 'o) -> Unsub<'o, NO> where Self: Sized
     {
         next.call(By::r(&self.v));
         ec.call_once(None);
@@ -19,12 +18,8 @@ impl<'o, V: 'o> Observable<'o, NO, Ref<V>, Ref<()>> for Of<V>
         Unsub::done()
     }
 
-    fn sub_dyn(&self,
-               next: Box<FnNext<NO, Ref<V>> + 'o>,
-               ec: Box<FnErrCompBox<NO, Ref<()>> + 'o>) -> Unsub<'o, NO>
-    {
-        self.sub(next, ec)
-    }
+    fn sub_dyn(&self, next: Box<FnNext<NO, Ref<V>> + 'o>, ec: Box<FnErrCompBox<NO, Ref<()>> + 'o>) -> Unsub<'o, NO>
+    { self.sub(next, ec) }
 }
 
 
@@ -42,7 +37,7 @@ mod test
         o.sub(|v:By<_>| { n.replace(*v); }, ());
         assert_eq!(n.get(), 123);
 
-        let o = o.into_dyn();
+        let o = Of::value_dyn(123);
         o.sub_dyn(box |v:By<_>| { n.replace(*v+1); }, box());
         assert_eq!(n.get(), 124);
     }

d0d9576d7b907774a54eea0254f9df7b91b91c5c
ying <ying@yingDev.com>
2018-10-17 23:09:33 +0800

    fmt

--- src/action.rs
+++ src/action.rs
@@ -20,10 +20,14 @@ pub unsafe trait ActBox<SS:YesNo, A>
 
 
 unsafe impl<'a, A, F: Fn(A)+'a> Act<NO, A> for F
-{ #[inline(always)] fn call(&self, v: A) { self(v) } }
+{
+    #[inline(always)] fn call(&self, v: A) { self(v) }
+}
 
 unsafe impl<A, F: Fn(A)+Send+Sync+'static> Act<YES, A> for F
-{ #[inline(always)] fn call(&self, v: A) { self(v) } }
+{
+    #[inline(always)] fn call(&self, v: A) { self(v) }
+}
 
 unsafe impl<'a, A, F: FnOnce(A)+'a> ActOnce<NO, A> for F
 {
@@ -35,20 +39,25 @@ unsafe impl<A, F: FnOnce(A)+Send+Sync+'static> ActOnce<YES, A> for F
     #[inline(always)] fn call_once(self, v: A) { self(v) }
 }
 
-
 unsafe impl<'a, SS: YesNo, A> Act<SS, A> for Box<Act<SS, A>+'a>
-{ #[inline(always)] fn call(&self, v: A) { self.as_ref().call(v) } }
+{
+    #[inline(always)] fn call(&self, v: A) { self.as_ref().call(v) }
+}
 
-
-unsafe impl<SS:YesNo, A, F> ActBox<SS, A> for F where F: ActOnce<SS, A>
-{ fn call_box(self: Box<F>, args: A)  { self.call_once(args) } }
+unsafe impl<SS:YesNo, A, F: ActOnce<SS, A>> ActBox<SS, A> for F
+{
+    #[inline(always)] fn call_box(self: Box<F>, args: A)  { self.call_once(args) }
+}
 
 unsafe impl<SS:YesNo, A> ActOnce<SS, A> for Box<ActBox<SS, A>>
-{ fn call_once(self, args: A) { self.call_box(args) } }
-
+{
+    #[inline(always)] fn call_once(self, args: A) { self.call_box(args) }
+}
 
 unsafe impl<SS:YesNo, A> Act<SS, A> for ()
-{ #[inline(always)] fn call(&self, v: A) {  } }
+{
+    #[inline(always)] fn call(&self, v: A) {  }
+}
 
 unsafe impl<SS:YesNo, A> ActOnce<SS, A> for ()
 {

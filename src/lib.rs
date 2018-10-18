5d88bfc4dceb7664aba8c34d60f770b764783369
ying <ying@yingDev.com>
2018-10-18 20:51:08 +0800

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -11,12 +11,7 @@ pub use crate::op::*;
 pub use crate::act::*;
 pub use crate::into_sendsync::*;
 pub use crate::observables::*;
-use std::rc::Rc;
-use std::sync::Arc;
 
-pub trait ActNext <'o, SS:YesNo, BY: RefOrVal> : for<'x> Act    <SS, By<'x, BY>>+'o {}
-pub trait ActEc   <'o, SS:YesNo, BY: RefOrVal> : for<'x> ActOnce<SS, Option<By<'x, BY>>>+'o {}
-pub trait ActEcBox<'o, SS:YesNo, BY: RefOrVal> : for<'x> ActBox <SS, Option<By<'x, BY>>>+'o {}
 
 pub trait Observable<'o, SS:YesNo, VBy: RefOrVal=Ref<()>, EBy: RefOrVal=Ref<()>>
 {
@@ -26,20 +21,19 @@ pub trait Observable<'o, SS:YesNo, VBy: RefOrVal=Ref<()>, EBy: RefOrVal=Ref<()>>
     fn sub_dyn(&self, next: Box<ActNext<'o, SS, VBy>>, ec: Box<ActEcBox<'o, SS, EBy>>) -> Unsub<'o, SS>;
 }
 
-pub unsafe trait IntoDyn<'o, SS:YesNo, VBy: RefOrVal, EBy: RefOrVal>
-    where Self: Observable<'o, SS, VBy, EBy> + Sized
+pub trait ActNext <'o, SS:YesNo, BY: RefOrVal> : for<'x> Act    <SS, By<'x, BY>>+'o {}
+pub trait ActEc   <'o, SS:YesNo, BY: RefOrVal> : for<'x> ActOnce<SS, Option<By<'x, BY>>>+'o {}
+pub trait ActEcBox<'o, SS:YesNo, BY: RefOrVal> : for<'x> ActBox <SS, Option<By<'x, BY>>>+'o {}
+
+pub unsafe trait IntoDyn<'o, SS, VBy, EBy> : Sized
 {
     #[inline(always)] fn into_dyn(self) -> Box<Self>  { box self }
 }
 
-unsafe impl<'a, 'o, SS:YesNo, VBy: RefOrVal, EBy: RefOrVal, O> IntoDyn<'o, SS, VBy, EBy> for O
-    where O: Observable<'o, SS, VBy, EBy> + Sized
-{}
-
-
-impl<'o, SS:YesNo, BY: RefOrVal, A: for<'x> Act<SS, By<'x, BY>>+'o> ActNext<'o, SS, BY> for A {}
-impl<'o, SS:YesNo, BY: RefOrVal, A: for<'x> ActOnce<SS, Option<By<'x, BY>>>+'o> ActEc<'o, SS, BY> for A {}
-impl<'o, SS:YesNo, BY: RefOrVal, A: for<'x> ActBox<SS, Option<By<'x, BY>>>+'o> ActEcBox<'o, SS, BY> for A {}
+unsafe impl<'a, 'o, SS:YesNo, VBy: RefOrVal, EBy: RefOrVal, O: Observable<'o, SS, VBy, EBy>> IntoDyn<'o, SS, VBy, EBy> for O {}
+impl<'o, SS:YesNo, BY: RefOrVal, A: for<'x> Act    <SS, By<'x, BY>>+'o>         ActNext<'o, SS, BY>  for A {}
+impl<'o, SS:YesNo, BY: RefOrVal, A: for<'x> ActOnce<SS, Option<By<'x, BY>>>+'o> ActEc<'o, SS, BY>    for A {}
+impl<'o, SS:YesNo, BY: RefOrVal, A: for<'x> ActBox <SS, Option<By<'x, BY>>>+'o> ActEcBox<'o, SS, BY> for A {}
 
 
 mod observables;

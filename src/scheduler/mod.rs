9f24f2af733384fcc77a085e0e56320dac4baff2
ying <ying@yingDev.com>
2018-10-24 15:25:16 +0800

    cleanup

--- src/scheduler/mod.rs
+++ src/scheduler/mod.rs
@@ -1,4 +1,19 @@
 use std::boxed::FnBox;
+use crate::*;
+
+pub trait Scheduler<SS:YesNo>
+{
+    fn schedule(&self, due: Option<::std::time::Duration>, act: impl SchActOnce<SS>) -> Unsub<'static, SS> where Self: Sized;
+}
+
+pub trait SchedulerPeriodic<SS:YesNo> : Scheduler<SS>
+{
+    fn schedule_periodic<S>(&self, period: ::std::time::Duration, act: impl SchActPeriodic<SS, S>) -> Unsub<'static, SS> where Self: Sized;
+}
+
+pub unsafe trait SchActPeriodic<SS:YesNo, S> : for<'x> Act<SS, &'x S, S> + 'static {}
+pub unsafe trait SchActOnce<SS:YesNo> : for<'x> ActOnce<SS, &'x Scheduler<SS>, Unsub<'static, SS>> + 'static {}
+pub unsafe trait SchActBox<SS:YesNo> : for<'x> ActBox<SS, &'x Scheduler<SS>, Unsub<'static, SS>> + 'static {}
 
 pub trait ThreadFactory
 {

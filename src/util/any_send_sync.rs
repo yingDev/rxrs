d43ef5ae0c7ac05293f1dfb990a3ce4111667ea2
ying <ying@yingDev.com>
2018-10-28 05:01:13 +0800

    ...

--- src/util/any_send_sync.rs
+++ src/util/any_send_sync.rs
@@ -6,12 +6,12 @@ unsafe impl<T> Sync for AnySendSync<T>{}
 
 impl<T> AnySendSync<T>
 {
-    pub unsafe fn new(t: T) -> AnySendSync<T> { AnySendSync{ t } }
-    pub fn into_inner(self) -> T { self.t }
+    #[inline(always)] pub unsafe fn new(t: T) -> AnySendSync<T> { AnySendSync{ t } }
+    #[inline(always)] pub fn into_inner(self) -> T { self.t }
 }
 
 impl<T> Deref for AnySendSync<T>
 {
     type Target = T;
-    fn deref(&self) -> &T { &self.t }
+    #[inline(always)] fn deref(&self) -> &T { &self.t }
 }

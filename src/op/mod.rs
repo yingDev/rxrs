d3f47106cf0f4fb94ab9cac70a673ceed78618ee
ying <ying@yingDev.com>
2018-10-30 17:02:14 +0800

    fix until

--- src/op/mod.rs
+++ src/op/mod.rs
@@ -1,11 +1,11 @@
 mod map;
 mod filter;
-//mod until;
+mod until;
 mod take;
 
 pub use self::map::*;
 pub use self::filter::*;
-//pub use self::until::*;
+pub use self::until::*;
 pub use self::take::*;
 
 #[cfg(test)]

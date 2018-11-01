22268f25ed4a9c9a562df1518f143fe442683060
ying <ying@yingDev.com>
2018-11-01 20:23:15 +0800

    fmt

--- src/observable.rs
+++ src/observable.rs
@@ -32,7 +32,6 @@ impl<'s, 'o, SS:YesNo, By: RefOrVal, EBy: RefOrVal> DynObservable<'s, 'o, SS, By
 
     pub fn subscribe_dyn(&self, next: Box<ActNext<'o, SS, By>>, err_or_comp: Box<ActEcBox<'o, SS, EBy>>) -> Unsub<'o, SS>
     { self.src.subscribe_dyn(next, err_or_comp) }
-
 }
 
 
@@ -58,12 +57,12 @@ for Arc<O>
     fn subscribe(&self, next: impl ActNext<'o, SS, By>, ec: impl ActEc<'o, SS, EBy>) -> Unsub<'o, SS> where Self: Sized
     { Arc::as_ref(self).subscribe(next, ec) }
 
-    fn into_dyn<'x>(self) -> DynObservable<'x, 'o, SS, By, EBy> where Self: Sized+'x
-    { DynObservable::from_arc(self) }
-
     #[inline(always)]
     fn subscribe_dyn(&self, next: Box<ActNext<'o, SS, By>>, ec: Box<ActEcBox<'o, SS, EBy>>) -> Unsub<'o, SS>
     { Arc::as_ref(self).subscribe_dyn(next, ec) }
+
+    fn into_dyn<'x>(self) -> DynObservable<'x, 'o, SS, By, EBy> where Self: Sized+'x
+    { DynObservable::from_arc(self) }
 }
 
 
@@ -75,12 +74,12 @@ for Box<O>
     fn subscribe(&self, next: impl ActNext<'o, SS, By>, ec: impl ActEc<'o, SS, EBy>) -> Unsub<'o, SS> where Self: Sized
     { Box::as_ref(self).subscribe(next, ec) }
 
-    fn into_dyn<'x>(self) -> DynObservable<'x, 'o, SS, By, EBy> where Self: Sized+'x
-    { DynObservable::from_box(self) }
-
     #[inline(always)]
     fn subscribe_dyn(&self, next: Box<ActNext<'o, SS, By>>, ec: Box<ActEcBox<'o, SS, EBy>>) -> Unsub<'o, SS>
     { Box::as_ref(self).subscribe_dyn(next, ec) }
+
+    fn into_dyn<'x>(self) -> DynObservable<'x, 'o, SS, By, EBy> where Self: Sized+'x
+    { DynObservable::from_box(self) }
 }
 
 impl<'s, 'o, SS:YesNo, By: RefOrVal, EBy: RefOrVal>
@@ -105,10 +104,10 @@ for Arc<dyn Observable<'o, SS, By, EBy>+'s>
     fn subscribe(&self, next: impl ActNext<'o, SS, By>, ec: impl ActEc<'o, SS, EBy>) -> Unsub<'o, SS> where Self: Sized
     { self.subscribe_dyn(box next, box ec) }
 
-    fn into_dyn<'x>(self) -> DynObservable<'x, 'o, SS, By, EBy> where Self: Sized+'x
-    { DynObservable::from_arc(self) }
-
     #[inline(always)]
     fn subscribe_dyn(&self, next: Box<ActNext<'o, SS, By>>, ec: Box<ActEcBox<'o, SS, EBy>>) -> Unsub<'o, SS>
     { Arc::as_ref(self).subscribe_dyn(next, ec) }
+
+    fn into_dyn<'x>(self) -> DynObservable<'x, 'o, SS, By, EBy> where Self: Sized+'x
+    { DynObservable::from_arc(self) }
 }

ea89101f7874d5cc6bdbffc76614390cfa6894d9
ying <ying@yingDev.com>
2018-11-01 20:19:10 +0800

    fmt

--- src/observer.rs
+++ src/observer.rs
@@ -61,36 +61,48 @@ for ()
     fn call(&self, _by: By::V) { }
 }
 
-unsafe impl<'o, SS:YesNo, BY: RefOrVal, N: ActNext<'o, SS, BY>, STOP: Act<SS, (), bool>+'o> ActNext<'o, SS, BY> for (N, STOP)
+unsafe impl<'o, SS:YesNo, BY: RefOrVal, N: ActNext<'o, SS, BY>, STOP: Act<SS, (), bool>+'o>
+ActNext<'o, SS, BY>
+for (N, STOP)
 {
     #[inline(always)]fn call(&self, v: BY::V) { self.0.call(v); }
     #[inline(always)]fn stopped(&self) -> bool {self.1.call(()) }
 }
 
 
-unsafe impl<'o, SS:YesNo, E, F:FnOnce(Option<E>)+'o> ActEc<'o, SS, Val<E>> for F
+unsafe impl<'o, SS:YesNo, E, F:FnOnce(Option<E>)+'o>
+ActEc<'o, SS, Val<E>>
+for F
 {
     fn call_once(self, e: Option<E>) { self(e) }
 }
 
-unsafe impl<'o, SS:YesNo, E, F:FnOnce(Option<&E>)+'o> ActEc<'o, SS, Ref<E>> for F
+unsafe impl<'o, SS:YesNo, E, F:FnOnce(Option<&E>)+'o>
+ActEc<'o, SS, Ref<E>>
+for F
 {
     fn call_once(self, e: Option<*const E>) { self(e.map(|e| unsafe{ &*e })) }
 }
 
 
-unsafe impl<'o, SS:YesNo, BY:RefOrVal+'o> ActNext<'o, SS, BY> for Box<ActNext<'o, SS, BY>>
+unsafe impl<'o, SS:YesNo, BY:RefOrVal+'o>
+ActNext<'o, SS, BY>
+for Box<ActNext<'o, SS, BY>>
 {
     fn call(&self, v: BY::V) { Box::as_ref(self).call(v) }
     fn stopped(&self) -> bool { Box::as_ref(self).stopped() }
 }
 
-unsafe impl<'o, SS:YesNo, BY:RefOrVal+'o> ActEc<'o, SS, BY> for Box<ActEcBox<'o, SS, BY>>
+unsafe impl<'o, SS:YesNo, BY:RefOrVal+'o>
+ActEc<'o, SS, BY>
+for Box<ActEcBox<'o, SS, BY>>
 {
     fn call_once(self, e: Option<BY::V>) { self.call_box(e) }
 }
 
-unsafe impl<'o, SS:YesNo, BY:RefOrVal+'o> ActEc<'o, SS, BY> for ()
+unsafe impl<'o, SS:YesNo, BY:RefOrVal+'o>
+ActEc<'o, SS, BY>
+for ()
 {
     fn call_once(self, _e: Option<BY::V>) {}
 }
@@ -110,9 +122,13 @@ impl<'o, SS:YesNo, By: RefOrVal, A> SSActNextWrap<SS, By, A>
     { SSActNextWrap{ next, PhantomData } }
 }
 
-unsafe impl<'o, SS:YesNo, By: RefOrVal, A: ActNext<'o, SS, By>> Ssmark<SS> for SSActNextWrap<SS, By, A> {}
+unsafe impl<'o, SS:YesNo, By: RefOrVal, A: ActNext<'o, SS, By>>
+Ssmark<SS>
+for SSActNextWrap<SS, By, A> {}
 
-unsafe impl<'o, SS:YesNo, By: RefOrVal+'o, A: ActNext<'o, SS, By>> ActNext<'o, SS, By> for SSActNextWrap<SS, By, A>
+unsafe impl<'o, SS:YesNo, By: RefOrVal+'o, A: ActNext<'o, SS, By>>
+ActNext<'o, SS, By>
+for SSActNextWrap<SS, By, A>
 {
     #[inline(always)] fn call(&self, v: <By as RefOrVal>::V) { self.next.call(v) }
     #[inline(always)] fn stopped(&self) -> bool { self.next.stopped() }
@@ -133,10 +149,16 @@ impl<By: RefOrVal, A> SSActEcWrap<By, A>
     pub fn into_inner(self) -> A { self.ec }
 }
 
-unsafe impl<'o, SS:YesNo, By: RefOrVal, A: ActEc<'o, SS, By>> Ssmark<SS> for SSActEcWrap<By, A> {}
+unsafe impl<'o, SS:YesNo, By: RefOrVal, A: ActEc<'o, SS, By>>
+Ssmark<SS>
+for SSActEcWrap<By, A> {}
 
 
-pub struct SsForward<SS:YesNo, Caps: Ssmark<SS>> { captures: Caps, PhantomData: PhantomData<SS> }
+pub struct SsForward<SS:YesNo, Caps: Ssmark<SS>>
+{
+    captures: Caps,
+    PhantomData: PhantomData<SS>
+}
 
 impl<SS:YesNo, Caps: Ssmark<SS>> SsForward<SS, Caps>
 {
@@ -148,9 +170,13 @@ impl<SS:YesNo, Caps: Ssmark<SS>> SsForward<SS, Caps>
     pub fn into_inner(self) -> Caps { self.captures }
 }
 
-unsafe impl<SS:YesNo, Caps: Ssmark<SS>> Ssmark<SS> for SsForward<SS, Caps> {}
+unsafe impl<SS:YesNo, Caps: Ssmark<SS>>
+Ssmark<SS>
+for SsForward<SS, Caps> {}
 
-impl<SS:YesNo, Caps: Ssmark<SS>> Deref for SsForward<SS, Caps>
+impl<SS:YesNo, Caps: Ssmark<SS>>
+Deref
+for SsForward<SS, Caps>
 {
     type Target = Caps;
     fn deref(&self) -> &Caps { &self.captures }
